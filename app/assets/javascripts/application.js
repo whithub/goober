ad3d6fe6c097f47c13c6061db05f0d00983c4db4
Whitney Hiemstra <whitcolorado@gmail.com>
2015-09-30 17:12:36 -0600

    update jquery scope to stop error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -37,7 +37,7 @@ $(document).ready(function() {
   }
 
 
-  if ($('body.rides.index').length > 0) {
+  if ($('body.user.rides.index').length > 0) {
 
     (function poll() {
       // http://techoctave.com/c7/posts/60-simple-long-polling-example-with-javascript-and-jquery/
